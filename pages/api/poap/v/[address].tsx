6e6409d26bc24d4389d7ea3bf5dd5570b00e5e2c
Kira <kira@glorylab.xyz>
2024-04-26 23:37:15 +0900

    Add moments param

--- pages/api/poap/v/[address].tsx
+++ pages/api/poap/v/[address].tsx
@@ -53,9 +53,10 @@ export default async function handler(request) {
     }
 
     let poaps = [];
+    let latestedMoments = [];
 
     if (request.method === 'POST') {
-        const { poaps: poapsParam, poapapikey } = await request.json();
+        const { poaps: poapsParam, latestedMoments: latestedMomentsParam , poapapikey } = await request.json();
 
         if (poapapikey !== process.env.POAP_API_KEY) {
             return new Response('Invalid API key', { status: 401 });
@@ -66,6 +67,8 @@ export default async function handler(request) {
         }
 
         poaps = poapsParam;
+        latestedMoments = latestedMomentsParam;
+        console.log('latestedMoments', latestedMoments);
     } else if (request.method === 'GET') {
         poaps = await getPoapsOfAddress(address);
     } else {
